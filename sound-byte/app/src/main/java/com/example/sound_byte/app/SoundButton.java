d1c31b331762d06845b0cf42b5935245a8cfd488
Paul Shi <mail.paulshi@gmail.com>
2014-04-19 18:06:06 -0400

    created provider, layout and receiver for soundbutton

---
+++ sound-byte/app/src/main/java/com/example/sound_byte/app/SoundButton.java
@@ -0,0 +1,20 @@
+package com.example.sound_byte.app;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by paulshi on 4/19/14.
+ */
+public class SoundButton extends AppWidgetProvider {
+
+    public void onEnabled(Context context){
+
+    }
+
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
+
+    }
+
+}
