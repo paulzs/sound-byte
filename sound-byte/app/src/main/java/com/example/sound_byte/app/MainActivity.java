3f0d61bc094236b7727cbb2249caa8a701102e2d
Paul Shi <mail.paulshi@gmail.com>
2014-04-19 19:08:20 -0400

    Added button media player behavior and raw directory

--- sound-byte/app/src/main/java/com/example/sound_byte/app/MainActivity.java
+++ sound-byte/app/src/main/java/com/example/sound_byte/app/MainActivity.java
@@ -1,36 +1,44 @@
 package com.example.sound_byte.app;
 
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.RemoteViews;
 
-public class MainActivity extends ActionBarActivity{
+/**
+ * Created by annakrasner & paulzs on 4/19/14.
+ */
 
-    Button main_button;
-    TextView text_view;
+public class MainActivity extends Activity{
 
+    Button soundbutton;
+    MediaPlayer mp = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        main_button = (Button) findViewById(R.id.main_button);
-        text_view = (TextView) findViewById(R.id.main_text_view);
-        main_button.setOnClickListener(new View.OnClickListener() {
+        soundbutton = (Button) findViewById(R.id.button);
+        mp = MediaPlayer.create(MainActivity.this,R.raw.oh_myyy);
+
+        soundbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                   text_view.setText("Hello World!");
+                  mp.start();
             }
         });
     }
 
-
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
@@ -50,6 +58,6 @@ public class MainActivity extends ActionBarActivity{
         }
         return super.onOptionsItemSelected(item);
     }
-
+*/
 
 }
--- sound-byte/app/src/main/java/com/example/sound_byte/app/SoundButton.java
+++ sound-byte/app/src/main/java/com/example/sound_byte/app/SoundButton.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 /**
- * Created by paulshi on 4/19/14.
+ * Created by annakrasner & paulzs on 4/19/14.
  */
 public class SoundButton extends AppWidgetProvider {
 
@@ -20,12 +20,9 @@ public class SoundButton extends AppWidgetProvider {
         // Get the layout for the App Widget and attach an on-click listener
         // to the button
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.soundbutton_layout);
-        Intent intent = new Intent(context, SoundButton.class);
+        Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
         views.setOnClickPendingIntent(R.id.button, pendingIntent);
-
-
     }
 
     @Override
@@ -35,15 +32,13 @@ public class SoundButton extends AppWidgetProvider {
 
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.soundbutton_layout);
         // find your TextView here by id here and update it.
-
         Toast.makeText(context, "Clicked!!", Toast.LENGTH_SHORT).show();
     }
 
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.soundbutton_layout);
-        Intent intent = new Intent(context, SoundButton.class);
+        Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
         views.setOnClickPendingIntent(R.id.button, pendingIntent);
     }
 
