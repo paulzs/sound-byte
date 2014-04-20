eeb8179121cbf4c2656c72451175fbf523672dd8
annakrasner <anna@mixamo.com>
2014-04-19 17:40:56 -0700

    working wdigetbutton yaaaay

--- sound-byte/app/src/main/java/com/example/sound_byte/app/MainActivity.java
+++ sound-byte/app/src/main/java/com/example/sound_byte/app/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.sound_byte.app;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,52 +18,53 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 /**
  * Created by annakrasner & paulzs on 4/19/14.
  */
 
-public class MainActivity extends Activity{
+public class MainActivity extends AppWidgetProvider {
 
-    Button soundbutton;
-    MediaPlayer mp = new MediaPlayer();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        soundbutton = (Button) findViewById(R.id.button);
-        mp = MediaPlayer.create(MainActivity.this,R.raw.oh_myyy);
-
-        soundbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                  mp.start();
-            }
-        });
-    }
-
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+   // Button soundbutton;
+    //MediaPlayer mp = new MediaPlayer();
+    public static String ACTION_WIDGET_RECEIVER = "ActionReceiverWidget";
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
+
+        for(int i=0; i<appWidgetIds.length; i++){
+            int currentWidgetId = appWidgetIds[i];
+            String url = "http://www.google.com";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setData(Uri.parse(url));
+            Intent soundIntent = new Intent(context, MainActivity.class);
+            soundIntent.setAction(ACTION_WIDGET_RECEIVER);
+           /* PendingIntent pending = PendingIntent.getActivity(context, 0,
+                    intent, 0);*/
+            PendingIntent pending = PendingIntent.getBroadcast(context, 0, soundIntent, 0);
+            RemoteViews views = new RemoteViews(context.getPackageName(),
+                    R.layout.activity_main);
+            views.setOnClickPendingIntent(R.id.main_button, pending);
+            appWidgetManager.updateAppWidget(currentWidgetId,views);
+            Toast.makeText(context, "widget added", Toast.LENGTH_SHORT).show();
         }
-        return super.onOptionsItemSelected(item);
+
     }
-*/
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+        if(intent.getAction().equals(ACTION_WIDGET_RECEIVER))
+        {
+            MediaPlayer mPlay = MediaPlayer.create(context, R.raw.oh_myyy);
+            mPlay.start();
+        }
+
+        super.onReceive(context,intent);
+    }
+
+
+
 
 }
